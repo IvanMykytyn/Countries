40ebc53f5589f418cfe6dec637fa30c129379fbb
VanekCheck <mukutunivan04@gmail.com>
2022-08-13 19:10:06 +0300

    fixed some bugs

--- src/components/_common/BorderCountry/BorderCountry.jsx
+++ src/components/_common/BorderCountry/BorderCountry.jsx
@@ -1,12 +1,14 @@
 import './border-country.styles.scss'
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const BorderCountry = ({ name }) => {
   return (
     <div className="border-country">
-      <a href={`/country/${name}`} className="border-country__link">
+      <Link to={`/country/${name}`} className="border-country__link">
         {name}
-      </a>
+      </Link>
     </div>
   )
 }
